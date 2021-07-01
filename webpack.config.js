0150905bb82549124bcf42ddb1a2c4175fa4e925
abaokbah <art-is-ablast@hotmail.com>
2021-07-01 15:18:09 +0300

    Initial deployment commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 
 const postCSSPlugins = [
@@ -8,23 +9,10 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
-module.exports = {
+
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
-        before: function(app, server) {
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port: 3000,
-        host: '0.0.0.0'
-    },
-    mode: 'development',
-    // watch: true,
     module: {
         rules: [
             {
@@ -34,3 +22,36 @@ module.exports = {
         ]
     }
 }
+
+if(currentTask == 'dev'){
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    }
+
+    config.devServer = {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    }
+
+    config.mode = "development"
+}
+
+if(currentTask == 'build'){
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+    config.mode = "production"
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+}
+
+module.exports = config
